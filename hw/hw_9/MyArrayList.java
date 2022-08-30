6ba79c5620845047842d642904b97207a8f62084
Sergey <sergdzd@gmail.com>
2022-08-30 17:05:33 +0300

    Update MyArrayList.java

--- hw/hw_9/MyArrayList.java
+++ hw/hw_9/MyArrayList.java
@@ -1,43 +1,71 @@
-package hw.hw_9;
 
-import javax.security.auth.login.CredentialNotFoundException;
-import java.util.ArrayList;
+package com.hw.hw_9;
 
 public class MyArrayList<E> {
-    private Object [] arrayList = new Object[10];
     final int MAX_SIZE = Integer.MAX_VALUE;
+    final int INIT_SIZE = 10;
+    private Object [] arrayList = new Object[INIT_SIZE];
     private int size;
 
 
-    public int size(){
-        return size;
-    }
-
     public boolean add(Object value){
-        if(size == MAX_SIZE) throw new ArrayIndexOutOfBoundsException();
+        Object[] newArrayList = new Object[size + INIT_SIZE];
+        if(size == MAX_SIZE) return false;
+        if(size == arrayList.length){
+            System.arraycopy(arrayList, 0, newArrayList, 0, size);
+            arrayList = newArrayList;
+        }
         arrayList[size++] = value;
         return true;
+    }
+
+    public void remove(int index){
+        if(index >= size && index < 0) throw new ArrayIndexOutOfBoundsException();
+        //System.arraycopy(a);
+        for(int i = index + 1; i < size; i++){
+            arrayList[i - 1] = arrayList[i];
+        }
+        arrayList[--size] = null;
     }
 
     public void clear(){
         for(int i = 0; i < size; i++){
             arrayList[i] = null;
         }
+        arrayList = new Object[INIT_SIZE];
         size = 0;
     }
 
-    public Object get(int index){
+    public int size(){
+        return size;
+    }
+
+    public E get(int index){
         if(index < size && index >= 0){
-            return arrayList[index];
+            return (E) arrayList[index];
         }
         throw new ArrayIndexOutOfBoundsException();
     }
+ 
 
-
-    //add(Object value) додає елемент в кінець
-    //remove(int index) видаляє елемент з вказаним індексом
-    //clear() очищає колекцію
-    //size() повертає розмір колекції
-    //get(int index) повертає елемент по індексу
+    public static void main(String[] args) {
+        MyArrayList<String> test = new MyArrayList<>();
+        test.add("test0");
+        test.add("test1");
+        test.add("test2");
+        test.add("test3");
+        test.add("test4");
+        test.add("test5");
+        test.add("test6");
+        test.add("test7");
+        test.add("test8");
+        test.add("test9");
+        test.add("test10");
+        test.add("test11");
+        System.out.println(test.get(5));
+        System.out.println(test.size());
+        test.remove(5);
+        test.clear();
+    }
 
 }
